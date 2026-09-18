3bdf36561f3876335724da0b566259c7e186f668
Akramhz299 <marmogobusiness@gmail.com>
2026-09-18 22:03:12 +0100

    Add initial Charming Lil Store WordPress WooCommerce theme

---
+++ wp-theme/charming-lil-store/index.php
@@ -0,0 +1,1 @@
+<?php get_header(); ?><main class="site-main section"><div class="wrap"><?php if(have_posts()):while(have_posts()):the_post();the_content();endwhile;else: ?><p>Aucun contenu.</p><?php endif; ?></div></main><?php get_footer(); ?>
