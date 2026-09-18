99269703648e11f776dd92a0307ba0caec90ae84
Akramhz299 <marmogobusiness@gmail.com>
2026-09-18 22:09:11 +0100

    Improve WooCommerce templates and theme customization

--- wp-theme/charming-lil-store/single-product.php
+++ wp-theme/charming-lil-store/single-product.php
@@ -1,1 +1,1 @@
-<?php get_header(); ?><main class="site-main"><?php while(have_posts()):the_post(); wc_get_template_part('content','single-product'); endwhile; ?></main><?php get_footer(); ?>
+<?php get_header(); woocommerce_content(); get_footer(); ?>
